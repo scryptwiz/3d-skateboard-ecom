96afeaf6eb8b5f021069fb3f27a8fc8d075e5a05
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-01-13 23:41:52 +0100

    added the skateboard physics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
 import { createClient } from '@/prismicio';
-import { PrismicNextImage } from '@prismicio/next';
+import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import { Logo } from './Logo';
+import { Bounded } from './Bounded';
+import { FooterPhysics } from './FooterPhysics';
+import { asImageSrc } from '@prismicio/client';
 
 export async function Footer () {
 	const client = createClient();
 	const settings = await client.getSingle("settings");
+
+	const boardTextureURLs = settings.data.footer_skateboards
+		.map((item) => asImageSrc(item.skateboard, { h: 600 }))
+		.filter((url): url is string => Boolean(url));
 
 	return (
 		<footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
@@ -16,8 +23,21 @@ export async function Footer () {
 					className="object-cover"
 					width={1200}
 				/>
+				<FooterPhysics
+					boardTextureURLs={boardTextureURLs}
+					className="absolute inset-0 overflow-hidden"
+				/>
 				<Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28" />
 			</div>
+			<Bounded as="nav">
+				<ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
+					{settings.data.navigation.map((item) => (
+						<li key={item.link.text} className="hover:underline">
+							<PrismicNextLink field={item.link} />
+						</li>
+					))}
+				</ul>
+			</Bounded>
 		</footer>
 	)
 }
